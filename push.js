9acc5db81a5ed86f6a2a2759ff5d1bae886b9b2c
evandrouzeda <49155191+evandrouzeda@users.noreply.github.com>
2020-04-09 17:50:59 -0300

    consertando erros

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto: evandrouzeda@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/fXKki0JFK5g:APA91bED7QfoZ6KgXxx10vNDCbO-5vUWL65mLVA-Ad8fxxhhaJ7FyhXCSyV92wG6fSWyrSk0Obqo_kF-fowXgDwGTquZ--jbw0yNPW6ldAi4AzPtaRv2ZfaqFbhVYN2mnKqIn7tKKPNP",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BB16vo9tY0zQwg29a5HujPre6XfNBlLjM0AZpw_TDBHslKY9_lzeU1kKFhlHdn1sOxTSHebDtWCuCN-8sqdhHis",
+        "auth": "L3G4s4IdiA79BV4_KyChCQ"
+    }
+};
 
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('test message', {});
+self.addEventListener('push', (e) => {
+    var options = {
+        body: 'Esse é o corpo da minha notificação',
+        icon: 'imagens/icone.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [{
+                action: 'explore',
+                title: 'Explore esse novo mundo',
+                icon: 'imagens/checkmark.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: 'imagens/xmark.png'
+            }
+        ]
+    };
+    e.waitUntil(self.registration.sendNotification('test message', {}));
 });
